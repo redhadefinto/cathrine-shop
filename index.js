3ccfb0565a70cd558c921aa98e829062b52e0e70
redhadefinto <redhadefinto28@gmail.com>
2023-08-02 21:52:16 +0700

    create welcome page

--- index.js
+++ index.js
@@ -5,12 +5,15 @@ const morgan = require("morgan");
 const cors = require("cors");
 const app = express();
 const { serverPort } = require("./src/configs/environment");
+const masterRouter = require("./src/routes");
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.use(morgan("dev"));
 app.use(cors());
+
+app.use(masterRouter);
 
 app.listen(serverPort, () => {
   console.log(`App listening to port : ${serverPort}`);
---
+++ src/routes/index.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const masterRouter = express.Router();
+
+const welcomeRouter = require("./welcome.route");
+
+masterRouter.use("/", welcomeRouter);
+
+module.exports = masterRouter;
