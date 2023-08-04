6a9a9ad8c25080b4eb19db62eae32590b4868e7b
redhadefinto <redhadefinto28@gmail.com>
2023-08-04 16:17:04 +0700

    create profile route

--- src/models/profile.model.js
+++ src/models/profile.model.js
@@ -2,7 +2,7 @@ const db = require("../configs/postgre");
 
 const getProfile = (id) => {
   return new Promise((resolve, reject) => {
-    let sqlQuery = `select email, phone, firstName, lastName, email, p.image, from users where id = $1`;
+    let sqlQuery = `select email, phone, firstName, lastName, image, from users where id = $1`;
     db.query(sqlQuery, [id], (err, result) => {
       if (err) {
         reject(err);
